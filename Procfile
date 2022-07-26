1cc5acec8e35f4aa507a34eb992e935361124d13
Gild-Shogi <ok2336.coffee@gmail.com>
2022-07-26 09:28:05 +0900

    SECRET_KEYno

---
+++ Procfile
@@ -0,0 +1,1 @@
+gunicorn hoge.wsgi --log-file
--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d%hz=edx*^3-k(*uy*uj(hh$*qtd%g57q2921w^p2kxr+0bzv3'
+env = environ.Env()
+if not os.getenv('HEROKU_ENV'):
+    env.read_env('.env')
+SECRET_KEY=env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
