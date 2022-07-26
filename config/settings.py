0265822dcc9c70c7b2c7595b52aecef7907ae713
Gild-Shogi <ok2336.coffee@gmail.com>
2022-07-26 13:26:11 +0900

    change host

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY=env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'recipe-compass.herokuapp.com']
 
 # Application definition
 
