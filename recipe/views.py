a147a38b4146ce17439f31db09934415a225b37b
Gild-Shogi <ok2336.coffee@gmail.com>
2022-07-27 10:08:08 +0900

    put template

--- recipe/views.py
+++ recipe/views.py
@@ -18,3 +18,6 @@ def view_json(request):
     json_open = open('../fixtures/recipes.json', 'r')
     recipes = json.load(json_open)
     return render(request, 'view_json.html', {'recipes': recipes})
+
+def template(request):
+    return render(request, 'recipe/test.html')
