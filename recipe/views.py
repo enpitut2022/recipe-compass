e28fdce2973b0ecd17fa787f33f781bd2b2b792c
Hattori Hideyuki <s1911429@s.tsukuba.ac.jp>
2022-07-28 11:28:58 +0900

    resultデモの作成,json画面の追加

--- recipe/views.py
+++ recipe/views.py
@@ -7,10 +7,15 @@ import json
 def index(request):
     json_open = open('./recipe/fixtures/recipes.json', 'r')
     recipes = json.load(json_open)
-    return render(request, 'recipe/test.html', {'recipes': recipes})
+    return render(request, 'recipe/card.html', {'recipes': recipes})
+
 
 def template(request):
     json_open = open('./recipe/fixtures/recipes.json', 'r')
     recipes = json.load(json_open)
-    return render(request, 'recipe/card.html', {'recipes': recipes})
+    return render(request, 'recipe/test.html', {'recipes': recipes})
 
+def result_sample(request):
+    json_open = open('./recipe/fixtures/recipes.json', 'r')
+    recipes = json.load(json_open)
+    return render(request, 'recipe/result/result-sample.html', {'recipes': recipes})
