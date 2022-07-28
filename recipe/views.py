3401092436f9fddb8ecaddc97eff13b8b70285c1
Gild-Shogi <ok2336.coffee@gmail.com>
2022-07-28 18:14:24 +0900

    json.loadのencode設定の削除

--- recipe/views.py
+++ recipe/views.py
@@ -7,7 +7,7 @@ import json
 
 def index(request):
     json_open = open('./recipe/fixtures/recipes.json', 'r', encoding="utf-8")
-    recipes = json.load(json_open, encoding="utf-8")
+    recipes = json.load(json_open)
     return render(request, 'recipe/card.html', {'recipes': recipes})
 
 # dbtest
